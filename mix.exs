2f05481d910763fc2eb7fdf51516427c9409c6ca
Steven Hornung <shornung@axlepayments.com>
2022-01-25 12:40:38 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Dwolla.Mixfile do
   def project do
     [
       app: :dwolla,
-      version: "1.0.0",
+      version: "1.0.1",
       description: @description,
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
